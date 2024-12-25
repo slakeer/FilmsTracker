ac8d741ce6a15abff0b41b79a7ed4b674e8853e1
vladfull <vladyslav.kukhar99@gmail.com>
2024-12-26 01:48:59 +0200

    ADD JWT Auth API

--- server/src/app.js
+++ server/src/app.js
@@ -1,20 +1,25 @@
 import express from 'express';
+import passport from "passport";
 // import bodyParser from 'body-parser';
 import movieRoutes from './routes/movie-routes.js';
 import genreRoutes from './routes/genre-routes.js';
 import watchedMoviesRoutes from './routes/watched-movie-routes.js';
 import favoriteMoviesRoutes from './routes/favorite-movie-routes.js';
 import reviewRoutes from './routes/review-routes.js';
+import authRoutes from './routes/auth-routes.js';
 
 import { swaggerDocs, swaggerUi } from './swagger-options.js';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/api/movies', movieRoutes);
 app.use('/api/genres', genreRoutes);
 app.use('/api/watched-movies', watchedMoviesRoutes);
 app.use('/api/favorite-movies', favoriteMoviesRoutes);
 app.use('/api/reviews', reviewRoutes);
+app.use('/api/auth', authRoutes);
 export default app;
---
+++ server/src/controllers/auth-controller.js
@@ -0,0 +1,50 @@
+import prisma from '../../prisma/prisma_client.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config("../.env");
+
+const register = async (req, res) => {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+        return res.status(400).json({ message: 'All fields are required' });
+    }
+
+    const exUser = await prisma.user.findFirst({
+        where: { email }
+    });
+    if (exUser) {
+        return res.status(400).json({message: 'User with such email already exists'});
+    }
+
+    const hashed = await bcrypt.hash(password, 10);
+    await prisma.user.create({
+        data: { 
+            name: username, 
+            email: email, 
+            password: hashed 
+        }
+    });
+
+    res.status(201).json({message: 'You are successfully registered!'})
+};
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({message: 'Email and password are required'});
+    }
+
+    const user = await prisma.user.findFirst((u) => u.email === email);
+    if (!user) {
+        return res.status(400).json({message: 'Invalid login or password'});
+    }
+    if(!bcrypt.compareSync(password, user.password)) {
+        return res.status(400).json({message: 'Invalid login or password'});
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, { expiresIn: '24h' });
+    res.status(200).json({message: 'Login successfull', token});
+};
+
+export { register, login };
