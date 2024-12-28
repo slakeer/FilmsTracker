1d48c13bb770e3c6dd1d6e2dc012bd4c33ba7100
Nikita Skriaha <nikitaskraga6@gmail.com>
2024-12-28 21:19:10 +0200

    linter-test

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -3,7 +3,7 @@ import pluginJs from '@eslint/js';
 import pluginReact from 'eslint-plugin-react';
 import prettier from 'eslint-config-prettier';
 import pluginPrettier from 'eslint-plugin-prettier';
-
+import pluginJest from 'eslint-plugin-jest';
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
   {
@@ -14,13 +14,14 @@ export default [
     }
   },
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { languageOptions: { globals: globals.browser } },
+  { languageOptions: { globals: { ...globals.browser, ...globals.jest } } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
   prettier, // Очищає конфлікти між ESLint і Prettier
   {
     plugins: {
-      prettier: pluginPrettier
+      prettier: pluginPrettier,
+      jest: pluginJest
     }
 
     // rules: {
